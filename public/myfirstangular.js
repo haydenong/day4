5b9856bff33d850833d8be002bd5f0d42980843e
hayden <hayden.nimo@ymail.com>
2016-06-30 22:41:04 +0800

    Second Commit

---
+++ public/myfirstangular.js
@@ -0,0 +1,13 @@
+/**
+ * Created by HF on 30/6/2016.
+ */
+(function () {
+    
+    var MyFirstApp = angular.module("MyFirstApp",[]);
+    
+    var MyFirstCtrl = function () {
+        
+    };
+    
+    MyFirstApp.controller("MyFirstCtrl", [MyFirstCtrl]);
+})();
