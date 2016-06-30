f88982f2de1e032df2f18ef6b44b4e8d102cea04
hayden <hayden.nimo@ymail.com>
2016-06-30 15:47:45 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by HF on 30/6/2016.
+ */
+
+var express = require("express");
+var app = express();
+var public = __dirname + "/public";
+
+app.use(express.static(public));
+app.use(express.static(__dirname + "/bower_components"));
+
+
+
+app.use(function (req,res) {
+    console.info("FILE NOT FOUND IN PUBLIC: %s", req.originalUrl);
+
+    res.redirect("/error.html");
+});
+
+var port = process.argv[2] || 3000;
+app.set("port", port);
+app.listen(app.get("port"), function () {
+    console.info("Web server started on port %d", app.get("port"));
+});
