774275f666dded7077bff1d2d65cb185fd2fe8aa
hayden <hayden.nimo@ymail.com>
2016-07-04 22:37:39 +0800

    first draft of angular validation

--- main.js
+++ main.js
@@ -9,7 +9,23 @@ var public = __dirname + "/public";
 app.use(express.static(public));
 app.use(express.static(__dirname + "/bower_components"));
 
+app.get("/register",function (req,res) {
+        var name = req.query.name;
+        var contact = req.query.contact;
+        var email= req.query.email;
+        var pax = req.query.pax;
+        var smoking = req.query.smoking;
+        var amenities = req.query.amenities;
+        var request = req.query.request;
 
+        console.info(req.query);
+        res.status(200).end();
+
+});
+
+app.get("/thankyou", function(req , res) {
+    res.redirect("thankyou1.html");
+});
 
 app.use(function (req,res) {
     console.info("FILE NOT FOUND IN PUBLIC: %s", req.originalUrl);
--- public/myfirstangular.js
+++ public/myfirstangular.js
@@ -2,12 +2,65 @@
  * Created by HF on 30/6/2016.
  */
 (function () {
-    
-    var MyFirstApp = angular.module("MyFirstApp",[]);
-    
-    var MyFirstCtrl = function () {
-        
+
+    var MyFirstApp = angular.module("MyFirstApp", []);
+
+    var MyFirstCtrl = function ($http, $window) {
+        var ctrl = this;
+        ctrl.name = "";
+        ctrl.contact = "";
+        ctrl.contactPattern = /^[0-9]{8}$/;
+        ctrl.email = "";
+        ctrl.pax = 1;
+        ctrl.request = "";
+        ctrl.amenities = {
+            pool: false,
+            wifi: false,
+            tv: false,
+            inRoomDining: false
+        };
+        ctrl.smoking = "no";
+        ctrl.status = {
+            message: "",
+            code: 0
+        };
+
+        ctrl.submit = function () {
+            console.info("name: %s", ctrl.name);
+            console.info("contact: %d", ctrl.contact);
+            console.info("email: %s", ctrl.email);
+            console.info("pax: %s", ctrl.pax);
+            console.info("smoking: %s", ctrl.smoking);
+
+            for (var i in ctrl.amenities) {
+                console.info("%s: %s", i, ctrl.amenities);
+            }
+
+            console.info("special request: %s", ctrl.request);
+
+
+            $http.get("/register", {
+                params: {
+                    name: ctrl.name,
+                    contact: ctrl.contact,
+                    email: ctrl.email,
+                    pax: ctrl.pax,
+                    smoking: ctrl.smoking,
+                    amenities: ctrl.amenities,
+                    request: ctrl.request
+                }
+            }).then(function () {
+                console.info("Success");
+                ctrl.status.code = 202;
+                $window.location = '/thankyou';
+            }).catch(function () {
+                console.info("Error");
+                ctrl.status.message = "Your registration failed";
+                ctrl.status.code = 400;
+            })
+
+        };
     };
-    
-    MyFirstApp.controller("MyFirstCtrl", [MyFirstCtrl]);
+
+    MyFirstApp.controller("MyFirstCtrl", ["$http", "$window", MyFirstCtrl]);
 })();
